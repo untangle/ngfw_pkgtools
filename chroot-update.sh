41ace2ca9d8e47e1f99610d3b5ab20e02f52f63c
Sebastien Delafond <sdelafond@gmail.com>
2007-11-16 01:02:10 +0000

    Also try upstream for alpha
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgtools@12796 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update.sh
+++ chroot-update.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Use the current distro to pull main, but use upstream from testing
+# Use the current distro to pull main, but use upstream from testing+alpha
 # (not everyone has upstream in his target distro)
 # --Seb
 
@@ -8,4 +8,5 @@ SOURCES=/etc/apt/sources.list
 
 echo deb http://mephisto/public/$1 $2 main >> ${SOURCES}
 echo deb http://mephisto/public/$1 testing upstream >> ${SOURCES}
+echo deb http://mephisto/public/$1 alpha upstream >> ${SOURCES}
 apt-get update
