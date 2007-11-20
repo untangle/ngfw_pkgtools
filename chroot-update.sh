550798b10d46e1e62ae7819f9ff3492f8c4b7cda
Sebastien Delafond <sdelafond@gmail.com>
2007-11-20 18:47:54 +0000

    As a temporary solution, also try to find upstream in sarge, no matter what the target repository is, since I haven't had time to set up upstream for the other repositories so fat...
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgtools@12922 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update.sh
+++ chroot-update.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-# Use the current distro to pull main, but use upstream from testing+alpha
+# Use the current distro to pull main, but use upstream from stage/testing+$1/testing+$1/alpha
 # (not everyone has upstream in his target distro)
 # --Seb
 
 SOURCES=/etc/apt/sources.list
 
 echo deb http://mephisto/public/$1 $2 main >> ${SOURCES}
+echo deb http://mephisto/public/sarge testing upstream >> ${SOURCES}
 echo deb http://mephisto/public/$1 testing upstream >> ${SOURCES}
 echo deb http://mephisto/public/$1 alpha upstream >> ${SOURCES}
 apt-get update
