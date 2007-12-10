c9f8dc5deafa9ae909c2651767ddcee11574929d
Kevin Steele <ksteele@untangle.com>
2007-12-10 22:44:50 +0000

    distribution part of path now uses DISTRIBUTION var
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@13549 8ebf8b4e-06f3-0310-961f-89f74618be54

--- make-build.sh
+++ make-build.sh
@@ -13,12 +13,12 @@ done
 
 ### a few variables
 FILE_IN="build-order.txt"
-PKGTOOLS_HOME="/localhome/qabuildbot/beasts/nightly_${TARGET_REP}/build/pkgtools"
+PKGTOOLS_HOME="/localhome/qabuildbot/beasts/${DISTRIBUTION}_${TARGET_REP}/build/pkgtools"
 results=0
 
 ### main
 # cd into the main trunk
-cd "/localhome/qabuildbot/beasts/nightly_${TARGET_REP}/build/${BUILD_DIR}"
+cd "/localhome/qabuildbot/beasts/${DISTRIBUTION}_${TARGET_REP}/build/${BUILD_DIR}"
 
 # first grab the content of the build-order.txt file
 build_dirs=()
