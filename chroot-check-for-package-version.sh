679f8c37a0e999a09a5881e509efbfc1c1029137
Sebastien Delafond <sdelafond@gmail.com>
2007-12-13 06:37:30 +0000

    only look at the highest available version
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@13674 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-check-for-package-version.sh
+++ chroot-check-for-package-version.sh
@@ -4,7 +4,9 @@ PACKAGE_NAME=$1
 VERSION=$2
 MARKER=$3
 
-output=`apt-cache show ${PACKAGE_NAME} | grep -E '^Version: '`
+# highest version available
+output=`apt-cache show ${PACKAGE_NAME} | grep -E '^Version: ' | head -1`
+
 if [ ! $? = 0 ] || echo "$output" | grep -vq "$VERSION" ; then
   echo $MARKER
 fi
