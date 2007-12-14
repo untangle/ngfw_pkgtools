faa47152d38a822724ea348392930fbdeccdf04e
Sebastien Delafond <sdelafond@gmail.com>
2007-12-14 05:40:54 +0000

    StrictHostKeyChecking=no, so it works evne if we never connected to mephisto before.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@13713 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mephisto-proxy.sh
+++ mephisto-proxy.sh
@@ -5,11 +5,11 @@
 
 KEY="./qabuildbot.dsa"
 REMOTE_USER="root@mephisto"
-SSH_KEY_OPTION="-i $KEY"
+SSH_OPTIONS="-i $KEY -o StrictHostKeyChecking=no"
 TMP_DIR=/tmp/mephisto-proxy_$$
 
 chmod 600 $KEY
-ssh $SSH_KEY_OPTION $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
-scp $SSH_KEY_OPTION *sh $REMOTE_USER:$TMP_DIR
-ssh $SSH_KEY_OPTION $REMOTE_USER "cd $TMP_DIR ; $@"
-ssh $SSH_KEY_OPTION $REMOTE_USER rm -fr $TMP_DIR
+ssh $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
+scp $SSH_OPTIONS *sh $REMOTE_USER:$TMP_DIR
+ssh $SSH_OPTIONS $REMOTE_USER "cd $TMP_DIR ; $@"
+ssh $SSH_OPTIONS $REMOTE_USER rm -fr $TMP_DIR
