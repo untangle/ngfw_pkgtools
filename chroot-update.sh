e045ccdd951b1a7e427d697775ec6c3a7fff7040
Sebastien Delafond <sdelafond@gmail.com>
2008-02-20 22:05:35 +0000

    Don't add the nightly source line if we're building for nightly.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@14784 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update.sh
+++ chroot-update.sh
@@ -8,7 +8,9 @@ SOURCES=/etc/apt/sources.list
 
 # for our own build-deps
 echo deb http://mephisto/public/$1 $2 main premium upstream internal >> ${SOURCES}
-echo deb http://mephisto/public/$1 nightly main premium upstream internal >> ${SOURCES}
+
+# also search in nightly
+[ $1 != nightly ] && echo deb http://mephisto/public/$1 nightly main premium upstream internal >> ${SOURCES}
 
 #echo deb http://mephisto/public/sarge testing upstream >> ${SOURCES}
 #echo deb http://mephisto/public/sarge alpha upstream >> ${SOURCES}
