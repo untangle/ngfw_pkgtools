f2358b1a19466330a85e0d2bb584c575c8e36237
Sebastien Delafond <sdelafond@gmail.com>
2008-03-25 15:25:17 +0000

    New getPlatform.sh, separated from incVersion.sh.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@15459 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ getPlatform.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+grep -q Debian /etc/issue && i=3 || i=2
+case `head -1 /etc/issue | awk "{ print \\$$i }"` in
+  lenny/sid) repository=sid ;;
+  4.0) repository=etch ;; 
+  3.1) repository=sarge ;;
+  7.04*) repository=feisty ;;
+  7.10*) repository=gutsy ;;
+  8.04*|hardy) repository=hardy ;;
+  *) echo "Couldn't guess your platform, giving up" ; exit 1 ;;
+esac
+
+echo $repository
