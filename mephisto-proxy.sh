cb27c2f98f0175a0512c29539aa195bddd3ce6b5
Sebastien Delafond <sdelafond@gmail.com>
2008-05-15 21:18:32 +0000

    2nd attempt at controlling mephisto-proxy.sh.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@16298 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mephisto-proxy.sh
+++ mephisto-proxy.sh
@@ -1,13 +1,12 @@
-#! /bin/bash
+#! /bin/bash -x
 
 # FIMXE: does it get any nastier than this ??
 # make that a CGI on mephisto, for fucksake...
 
 REMOTE_USER="mephisto"
-SSH_OPTIONS="-i ~/.ssh/id_rsa -o StrictHostKeyChecking=no"
+SSH_OPTIONS="-o StrictHostKeyChecking=no"
 TMP_DIR=/tmp/mephisto-proxy_$$
 
-chmod 600 $KEY
 ssh $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
 scp $SSH_OPTIONS -q *sh $REMOTE_USER:$TMP_DIR
 ssh $SSH_OPTIONS $REMOTE_USER "cd $TMP_DIR ; $@"
