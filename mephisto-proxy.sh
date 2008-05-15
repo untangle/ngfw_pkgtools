d1826851c5c0ada19d60bfd21adc1051f4aa4712
Sebastien Delafond <sdelafond@gmail.com>
2008-05-15 21:12:34 +0000

    Attempt to control the collateral damages caused by the butt-ugly mephisto-proxy.sh.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@16297 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mephisto-proxy.sh
+++ mephisto-proxy.sh
@@ -3,9 +3,8 @@
 # FIMXE: does it get any nastier than this ??
 # make that a CGI on mephisto, for fucksake...
 
-KEY="./qabuildbot.dsa"
-REMOTE_USER="root@mephisto"
-SSH_OPTIONS="-i $KEY -o StrictHostKeyChecking=no"
+REMOTE_USER="mephisto"
+SSH_OPTIONS="-i ~/.ssh/id_rsa -o StrictHostKeyChecking=no"
 TMP_DIR=/tmp/mephisto-proxy_$$
 
 chmod 600 $KEY
