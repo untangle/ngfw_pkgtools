22e9af2c4a635a423d026334b7fbd00b6d47b542
Sebastien Delafond <sdelafond@gmail.com>
2008-10-06 18:55:50 +0000

    We now need to check upstream too for package existence.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@19415 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update-existence.sh
+++ chroot-update-existence.sh
@@ -4,7 +4,7 @@ REPOSITORY=$1
 DISTRIBUTION=$2
 
 # we only need this one
-echo deb http://mephisto/public/$REPOSITORY $DISTRIBUTION main premium > /etc/apt/sources.list
+echo deb http://mephisto/public/$REPOSITORY $DISTRIBUTION main premium upstream > /etc/apt/sources.list
 
 apt-get update
 
