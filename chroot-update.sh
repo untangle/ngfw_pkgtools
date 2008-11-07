31a3855dd203fd0b4f1d9be3ade1ca48f733fbec
Sebastien Delafond <sdelafond@gmail.com>
2008-11-07 01:30:46 +0000

    Whether now or later, you'll have to surrender.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@20124 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update.sh
+++ chroot-update.sh
@@ -9,6 +9,12 @@ DEBIAN_MIRROR=http://debian/debian
 UBUNTU_MIRROR=http://ubuntu/ubuntu
 
 if [ $# = 0 ] ; then
+  if grep -q debian $SOURCES ; then
+    grep -q "non-free" $SOURCES || perl -i -pe 's/main$/main contrib non-free/' $SOURCES
+  else
+    grep -q "universe" $SOURCES || perl -i -pe 's/main$/main universe multiverse/' $SOURCES
+  fi
+
   apt-get update
   DEBIAN_FRONTEND=noninteractive apt-get install -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge debhelper aptitude
   DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge
@@ -43,12 +49,6 @@ case $DISTRIBUTION in
       *) addSource "http://mephisto/public/$REPOSITORY nightly${branch} main premium upstream"
     esac ;;
 esac
-
-if grep -q debian $SOURCES ; then
-  grep -q "non-free" $SOURCES || perl -i -pe 's/main$/main contrib non-free/' $SOURCES
-else
-  grep -q "universe" $SOURCES || perl -i -pe 's/main$/main universe multiverse/' $SOURCES
-fi
 
 apt-get -q update
 
