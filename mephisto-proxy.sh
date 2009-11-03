91c50bb256ce8a5b15908ad8befa211c00e8995e
Sebastien Delafond <sdelafond@gmail.com>
2009-11-03 18:41:46 +0000

    Merging      25058 From         svn://chef/branch/prod/release-7.1/pkgtools/ Timestamp    2009-11-03 10:41:04 -0800 Log message: 		Exit with the return code of the remote command.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@25059 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mephisto-proxy.sh
+++ mephisto-proxy.sh
@@ -10,4 +10,7 @@ TMP_DIR=/tmp/mephisto-proxy_$$
 ssh $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
 scp $SSH_OPTIONS -q *sh $REMOTE_USER:$TMP_DIR
 ssh $SSH_OPTIONS $REMOTE_USER "cd $TMP_DIR ; $@"
+rc=$?
 ssh $SSH_OPTIONS $REMOTE_USER rm -fr $TMP_DIR
+
+exit $rc
