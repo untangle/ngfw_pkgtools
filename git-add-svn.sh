ee4a034ccfe19ed9d309461ebf0120d79e7bd8af
Sebastien Delafond <sdelafond@gmail.com>
2010-04-15 09:40:23 +0000

    Functional.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@26291 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ git-add-svn.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+set -e
+
+SVN_SERVER="chef"
+SVN_BASE_DIR="work"
+
+GIT_DEFAULT_BRANCH="origin/master"
+GIT_REMOTE_TRUNK=".git/refs/remotes/trunk"
+
+# main
+module=$(git config --get remote.origin.url | perl -pe 's|.*/(.+)\.git|$1|')
+
+dir=${SVN_BASE_DIR}/$module
+
+git svn init -T $dir \
+             -b branch/prod/*/$dir \
+             -t tags/*/$dir \
+             svn://$SVN_SERVER
+
+git show $GIT_BRANCH | awk '{print $2 ; exit}' >| $GIT_REMOTE_TRUNK
+
+git svn fetch -r HEAD
