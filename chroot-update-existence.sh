14bfc5d5be4db2a5d4ec27f9ace3cd36cde4eb7d
Sebastien Delafond <sdelafond@gmail.com>
2011-12-08 10:28:16 +0000

    [builds] If nightly ran before chaos, take advantage of this too
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@30367 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-update-existence.sh
+++ chroot-update-existence.sh
@@ -5,10 +5,14 @@ DISTRIBUTION=$2
 
 # corresponding chaos distribution
 chaos=$(echo $DISTRIBUTION | perl -pe 's/nightly/chaos/')
+# corresponding nightly distribution
+nightly=$(echo $DISTRIBUTION | perl -pe 's/chaos/nightly/')
 
 echo deb http://mephisto/public/$REPOSITORY $DISTRIBUTION main premium upstream internal >| /etc/apt/sources.list
 if [ $chaos != $DISTRIBUTION ] ; then
   echo deb http://mephisto/public/$REPOSITORY $chaos main premium upstream internal >> /etc/apt/sources.list
+elif [ $nightly != $DISTRIBUTION ] ; then
+  echo deb http://mephisto/public/$REPOSITORY $nightly main premium upstream internal >> /etc/apt/sources.list
 fi
 apt-get update
 apt-get install --yes --force-yes apt-show-versions
