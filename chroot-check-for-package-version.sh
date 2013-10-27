73d99a743b78eb15f67e117a7a2e4a3b97e3e205
Sebastien Delafond <sdelafond@gmail.com>
2013-10-27 15:17:00 +0000

    [pkgtools] Logging info to stderr
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/internal/pkgtools@36271 8ebf8b4e-06f3-0310-961f-89f74618be54

--- chroot-check-for-package-version.sh
+++ chroot-check-for-package-version.sh
@@ -9,9 +9,9 @@ if [ $# != 3 ] ; then
   exit 1
 fi
 
-echo "[existence] Looking for $PACKAGE_NAME version $VERSION for $DISTRIBUTION"
-echo "[existence] sources.list is :"
-cat /etc/apt/sources.list
+echo "[existence] Looking for $PACKAGE_NAME version $VERSION for $DISTRIBUTION" >&2
+echo "[existence] sources.list is :" >&2
+cat /etc/apt/sources.list >&2
 
 str="$PACKAGE_NAME is available in"
 
@@ -23,13 +23,12 @@ nightly=$(echo $DISTRIBUTION | perl -pe 's/chaos/nightly/')
 apt-get install --yes --force-yes apt-show-versions
 
 # all distributions containing that version
-#apt-show-versions -p $PACKAGE_NAME -a >&2
-echo "[existence] apt-show-versions result:"
-apt-show-versions -p $PACKAGE_NAME -a
+echo "[existence] apt-show-versions result:" >&2
+apt-show-versions -p $PACKAGE_NAME -a >&2
 
 output=$(apt-show-versions -p $PACKAGE_NAME -a | awk '/^'"$PACKAGE_NAME ${VERSION/+/\+}"'/ {print $3}')
-echo "[existence] Matching line:"
-echo $output
+echo "[existence] Matching line:" >&2
+echo $output >&2
 
 if echo "$output" | grep -q $DISTRIBUTION ; then
   echo $str $DISTRIBUTION
