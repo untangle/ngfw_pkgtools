59639108a6984db77a917eb5cc967adcf3dd4fbc
Sébastien Delafond <sdelafond@gmail.com>
2017-01-04 13:14:07 +0100

    Change versioning scheme (NGFW-10258 #code-completed)

--- incVersion.sh
+++ incVersion.sh
@@ -75,7 +75,7 @@ if [ -z "$version" ] ; then
   # this is the base version; it will be tweaked a bit if need be:
   # - append a local modification marker is we're not up to date
   # - prepend the upstream version if UNTANGLE-KEEP-UPSTREAM-VERSION exists
-  baseVersion=`cat $versionFile`~vcs${timestamp}.${revision}.${branch}
+  baseVersion=$(cat $versionFile).${timestamp}.${revision}
 
   if [ -f UNTANGLE-KEEP-UPSTREAM-VERSION ] ; then
     baseVersion=${previousUpstreamVersion}+${baseVersion}
