cf85261454cc512b735846a68d89d4ce596304d6
Sébastien Delafond <sdelafond@gmail.com>
2017-07-12 08:31:12 +0200

    Tell getPlatform that 9.0 is stretch

--- getPlatform.sh
+++ getPlatform.sh
@@ -3,11 +3,12 @@
 grep -q Debian /etc/issue && i=3 || i=2
 case `head -1 /etc/issue | awk "{ print \\$$i }"` in
   *sid) repository=sid ;;
-  8.0) repository=jessie ;; 
-  7.0) repository=wheezy ;; 
-  6.0) repository=squeeze ;; 
-  5.0) repository=lenny ;; 
-  4.0) repository=etch ;; 
+  9.0) repository=stretch ;;
+  8.0) repository=jessie ;;
+  7.0) repository=wheezy ;;
+  6.0) repository=squeeze ;;
+  5.0) repository=lenny ;;
+  4.0) repository=etch ;;
   3.1) repository=sarge ;;
   7.04*) repository=feisty ;;
   7.10*) repository=gutsy ;;
