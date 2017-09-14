8122482a2127c38006ef6e59bdb5d06307eb1140
Sébastien Delafond <sdelafond@gmail.com>
2017-09-14 15:17:27 +0200

    Make sure package-server-proxy.sh copies over its target script

--- package-server-proxy.sh
+++ package-server-proxy.sh
@@ -3,14 +3,15 @@
 # FIMXE: does it get any nastier than this ??
 # make that a CGI on package-server, for fucksake...
 
-REMOTE_USER="package-server"
+PKGTOOLS_DIR=$(readlink -f $(dirname $0))
+REMOTE_HOST="package-server.untangle.int"
 SSH_OPTIONS="-o StrictHostKeyChecking=no"
 TMP_DIR=/tmp/package-server-proxy_$$
 
-ssh $SSH_OPTIONS $REMOTE_USER "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
-scp $SSH_OPTIONS -q *sh $REMOTE_USER:$TMP_DIR
-ssh $SSH_OPTIONS $REMOTE_USER "cd $TMP_DIR ; $@"
+ssh $SSH_OPTIONS $REMOTE_HOST "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
+scp $SSH_OPTIONS -q $PKGTOOLS_DIR/$1 $REMOTE_HOST:$TMP_DIR
+ssh $SSH_OPTIONS $REMOTE_HOST "cd $TMP_DIR ; $@"
 rc=$?
-ssh $SSH_OPTIONS $REMOTE_USER rm -fr $TMP_DIR
+ssh $SSH_OPTIONS $REMOTE_HOST rm -fr $TMP_DIR
 
 exit $rc
