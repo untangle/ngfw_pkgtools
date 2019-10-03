93056ff29343e595f670544ab6e0c3e13fdde384
Sébastien Delafond <sdelafond@gmail.com>
2019-10-03 15:09:03 +0200

    compare-branches.py: reindent to 4 spaces

--- compare-branches.py
+++ compare-branches.py
@@ -6,7 +6,7 @@ import argparse, json, os, requests, subprocess, sys
 GITHUB_BASE_URL = 'https://api.github.com/repos/untangle/{repository}'
 GITHUB_COMPARE_URL = GITHUB_BASE_URL + '/compare/{branchTo}...{branchFrom}'
 GITHUB_MERGE_URL = GITHUB_BASE_URL + '/merges'
-GITHUB_HEADERS = {'Accept' : 'application/vnd.github.loki-preview+json' } 
+GITHUB_HEADERS = {'Accept' : 'application/vnd.github.loki-preview+json' }
 GITHUB_USER = 'untangle-bot'
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 HEADER1_TPL = "{branchFrom} vs. {branchTo}"
@@ -48,52 +48,52 @@ parser.add_argument('--branch-to', dest='branchTo',
 
 # functions
 def getCompareUrl(repository, branchFrom, branchTo):
-  return GITHUB_COMPARE_URL.format(repository=repository,
-                                   branchFrom=branchFrom,
-                                   branchTo=branchTo)
+    return GITHUB_COMPARE_URL.format(repository=repository,
+                                     branchFrom=branchFrom,
+                                     branchTo=branchTo)
 
 def getJson(url, headers, auth, postData = None):
-  if postData:
-    r = requests.post(url, headers = headers, auth = auth, json = postData)
-  else:
-    r = requests.get(url, headers = headers, auth = auth)
-  
-  sc = r.status_code
-  if sc == 401:
-    print("Couldn't authenticate to GitHub, you need to export a valid GITHUB_TOKEN")
-    sys.exit(1)
-  elif sc == 204:
-    jsonData = None
-  else:
-    jsonData = r.json()
+    if postData:
+        r = requests.post(url, headers = headers, auth = auth, json = postData)
+    else:
+        r = requests.get(url, headers = headers, auth = auth)
 
-  return sc, jsonData
+    sc = r.status_code
+    if sc == 401:
+        print("Couldn't authenticate to GitHub, you need to export a valid GITHUB_TOKEN")
+        sys.exit(1)
+    elif sc == 204:
+        jsonData = None
+    else:
+        jsonData = r.json()
+
+    return sc, jsonData
 
 def merge(repository, branchFrom, branchTo):
-  url = GITHUB_MERGE_URL.format(repository=repository)
-  postData = { 'base':branchTo, 'head':branchFrom, 'commit_message':'Merged by Jenkins'}
-  sc, jsonData = getJson(url, GITHUB_HEADERS, (GITHUB_USER, GITHUB_TOKEN), postData = postData)
+    url = GITHUB_MERGE_URL.format(repository=repository)
+    postData = { 'base':branchTo, 'head':branchFrom, 'commit_message':'Merged by Jenkins'}
+    sc, jsonData = getJson(url, GITHUB_HEADERS, (GITHUB_USER, GITHUB_TOKEN), postData = postData)
 
-  if sc == 204:
-    success = True
-    status = 'SKIPPED: no need to merge'
-  elif sc == 201:
-    success = True
-    status = 'DONE: commitId=' + jsonData['sha']
-  else:
-    success = False
-    status = 'FAILED: conflicts'
+    if sc == 204:
+        success = True
+        status = 'SKIPPED: no need to merge'
+    elif sc == 201:
+        success = True
+        status = 'DONE: commitId=' + jsonData['sha']
+    else:
+        success = False
+        status = 'FAILED: conflicts'
 
-  return success, status
+    return success, status
 
 def compare(repository, branchFrom, branchTo):
-  url = getCompareUrl(repository, branchFrom, branchTo)
-  sc, jsonData = getJson(url, GITHUB_HEADERS, (GITHUB_USER, GITHUB_TOKEN))
+    url = getCompareUrl(repository, branchFrom, branchTo)
+    sc, jsonData = getJson(url, GITHUB_HEADERS, (GITHUB_USER, GITHUB_TOKEN))
 
-  ahead, behind = [ int(jsonData[x]) for x in ('ahead_by', 'behind_by') ]
-  extra = "!!! Need to merge !!!" if ahead > 0 else ""
+    ahead, behind = [ int(jsonData[x]) for x in ('ahead_by', 'behind_by') ]
+    extra = "!!! Need to merge !!!" if ahead > 0 else ""
 
-  return ahead, behind, extra
+    return ahead, behind, extra
 
 # main
 args = parser.parse_args()
@@ -103,20 +103,20 @@ rc = 0
 print(HEADER1_TPL.format(branchFrom=branchFrom, branchTo=branchTo))
 
 for repository in NGFW_REPOSITORIES:
-  print()
-  print(HEADER2_TPL.format(repository=repository))
+    print()
+    print(HEADER2_TPL.format(repository=repository))
 
-  if args.merge:
-    success, status = merge(repository, branchFrom, branchTo)
-    print(OUTPUT_MERGE_TPL.format(status=status))
-    if success:
-      continue
-    else:
-      rc = 1
+    if args.merge:
+        success, status = merge(repository, branchFrom, branchTo)
+        print(OUTPUT_MERGE_TPL.format(status=status))
+        if success:
+            continue
+        else:
+            rc = 1
 
-  ahead, behind, extra = compare(repository, branchFrom, branchTo)
-  print(OUTPUT_COMPARE_TPL.format(ahead=ahead,
-                                  behind=behind,
-                                  extra=extra))
+    ahead, behind, extra = compare(repository, branchFrom, branchTo)
+    print(OUTPUT_COMPARE_TPL.format(ahead=ahead,
+                                    behind=behind,
+                                    extra=extra))
 
 sys.exit(rc)
