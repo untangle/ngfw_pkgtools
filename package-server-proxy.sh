7786d36ac941d053e512248f34895ab5d4e37743
Sébastien Delafond <sdelafond@gmail.com>
2020-02-12 12:41:40 +0100

    Use buildbot user to connect to package-server, with the key supplied by docker-compose

--- package-server-proxy.sh
+++ package-server-proxy.sh
@@ -4,18 +4,20 @@
 # make that a CGI on package-server, for fucksake...
 
 PKGTOOLS_DIR=$(readlink -f $(dirname $0))
+REMOTE_USER="buildbot"
 REMOTE_HOST="package-server"
-SSH_OPTIONS="-o StrictHostKeyChecking=no"
+REMOTE=${REMOTE_USER}@$REMOTE_HOST
+SSH_OPTIONS="-o StrictHostKeyChecking=no -i /tmp/travis-buildbot.rsa"
 TMP_DIR=/tmp/package-server-proxy_$$
 
 LOCAL_TARGET_SCRIPT=$1
 shift
 REMOTE_TARGET_SCRIPT="./$(basename $LOCAL_TARGET_SCRIPT)"
 
-ssh $SSH_OPTIONS $REMOTE_HOST "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
-scp $SSH_OPTIONS -q $LOCAL_TARGET_SCRIPT $REMOTE_HOST:$TMP_DIR
-ssh $SSH_OPTIONS $REMOTE_HOST "cd $TMP_DIR ; $REMOTE_TARGET_SCRIPT $@"
+ssh $SSH_OPTIONS $REMOTE "rm -fr $TMP_DIR ; mkdir $TMP_DIR"
+scp $SSH_OPTIONS -q $LOCAL_TARGET_SCRIPT $REMOTE:$TMP_DIR
+ssh $SSH_OPTIONS $REMOTE "cd $TMP_DIR ; $REMOTE_TARGET_SCRIPT $@"
 rc=$?
-ssh $SSH_OPTIONS $REMOTE_HOST rm -fr $TMP_DIR
+ssh $SSH_OPTIONS $REMOTE rm -fr $TMP_DIR
 
 exit $rc
