617eb9374af1df9f01951f829d10de97bbe2eeff
Sébastien Delafond <sdelafond@gmail.com>
2021-10-20 11:15:22 +0200

    lib: load repositories.yaml from anywhere

--- lib/constants.py
+++ lib/constants.py
@@ -1,12 +1,14 @@
 import os
 import os.path as osp
 import shutil
+import sys
+
 
 WORK_DIR = osp.join(os.getenv('HOME'), 'tmp', 'pkgtools-workdir')
 # FIXME: separate cleanup() method, callable from other scripts
 shutil.rmtree(WORK_DIR, ignore_errors=True)
 
-YAML_REPOSITORY_INFO = 'repositories.yaml'
+YAML_REPOSITORY_INFO = osp.join(os.path.dirname(sys.argv[0]), 'repositories.yaml')
 
 NETBOOT_USER = 'buildbot'
 NETBOOT_HOST = 'netboot-server.untangle.int'
