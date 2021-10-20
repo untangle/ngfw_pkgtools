8422f55693e9aeb82d9f080de64c4ecde76a48f6
Sébastien Delafond <sdelafond@gmail.com>
2021-10-20 11:15:22 +0200

    lib: correctly handle type error in command-line arguments

--- lib/__init__.py
+++ lib/__init__.py
@@ -7,9 +7,12 @@ from .constants import *
 def simple_version(version):
     if re.match(r'^\d+\.\d+$', version):
         return version
+    else:
+        raise ValueError("not a valid simple version (x.y)")
 
 
 def full_version(version):
     if re.match(r'^\d+\.\d+\.\d+$', version):
         return version
-
+    else:
+        raise ValueError("not a valid full version (x.y.z)")
